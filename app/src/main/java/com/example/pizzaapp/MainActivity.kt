62f8bbe1c8bd0c7ebdf055d5404366dc49931786
Priya Boopathi <priyanandh06@gmail.com>
2024-04-10 13:47:27 -0700

    Added preview

--- app/src/main/java/com/example/pizzaapp/MainActivity.kt
+++ app/src/main/java/com/example/pizzaapp/MainActivity.kt
@@ -3,8 +3,16 @@ package com.example.pizzaapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.pizzaapp.UIComposables.ToppingCell
 import com.example.pizzaapp.ui.theme.PizzaAppTheme
 
@@ -29,6 +37,12 @@ fun onCheckboxChange(isChecked: Boolean) {
 @Composable
 fun GreetingPreview() {
     PizzaAppTheme {
-       // Greeting("Android")
+            Row {
+                Checkbox(checked = true, onCheckedChange = {/*something */})
+                Column() {
+                    Text(text = "Pineapple")
+                    Text(text = "Whole pizza")
+                }
+            }
     }
 }
